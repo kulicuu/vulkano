11b8f5a1f7ee21122f616f3aa88661ee1862abe4
Wylie Кулик <kulik.wylie@protonmail.com>
2020-03-12 00:55:53 -0500

    ...

--- examples/src/bin/scratch_000/main.rs
+++ examples/src/bin/scratch_000/main.rs
@@ -28,11 +28,18 @@ use tobj;
 
 
 #[derive(Default, Copy, Clone)]
-
-
 struct Vertex {
     position: (f32, f32, f32)
 }
+
+vulkano::impl_vertex!(Vertex, position);
+
+#[derive(Default, Copy, Clone)]
+pub struct Normal {
+    normal: (f32, f32, f32)
+}
+
+vulkano::impl_vertex!(Normal, normal);
 
 fn main() {
 
@@ -97,7 +104,7 @@ fn main() {
 
 
 
-    let mut counter = 0;
+    // let mut counter = 0;
 
     // we'll need the count at some point, might as well try the upfront approach first.
 
@@ -107,50 +114,42 @@ fn main() {
     // but this number doesn't help.
 
     let mut vertices : Vec<Vertex> = Vec::new();
+    let mut normals : Vec<Normal> = Vec::new();
 
     for (idx, model) in models.iter().enumerate() {
         let mesh = &model.mesh;
-        println!("mesh counter: {:?}", &counter);
-        println!("idx {:?}", idx);
+        // println!("mesh counter: {:?}", &counter);
+        // println!("idx {:?}", idx);
 
 
-        if counter == 0 {
-            println!("Hopefully you only see this line once.");
+        // println!("123123123: {:?}", mesh.positions);
 
-
-            // we need to roll through this data three elements at a time making
-            // a Vertex type
-
-
-            println!("123123123: {:?}", mesh.positions);
-
-            let vertices_count = (&mesh.positions.iter().count() + 1) / 3;
-            for jdx in 0..vertices_count {
-                let cursor = &mesh.positions[(jdx * 3)..((jdx * 3) + 3)];
-                println!("aeuauejjj :: 3939 {:?}", cursor[0]);
-                vertices.push(Vertex { position: (cursor[0], cursor[1], cursor[2]) });
-            }
-
-
+        let vertices_count = (&mesh.positions.iter().count() + 1) / 3;
+        for jdx in 0..vertices_count {
+            let cursor = &mesh.positions[(jdx * 3)..((jdx * 3) + 3)];
+            let normal_cursor = &mesh.normals[(jdx * 3)..((jdx * 3) + 3)];
+            // println!("aeuauejjj :: 3939 {:?}", cursor[0]);
+            vertices.push(Vertex { position: (cursor[0], cursor[1], cursor[2]) });
+            normals.push(Normal { normal: (normal_cursor[0], normal_cursor[1], normal_cursor[2])});
         }
 
+        // if counter == 0 {
+        //     println!("Hopefully you only see this line once.");
+        //     // we need to roll through this data three elements at a time making
+        //     // a Vertex type
+        // }
 
-        counter = &counter + 1;
+
+        // counter = &counter + 1;
     }
 
-
-
+    println!("888484 {:?}", vertices.iter().count());
+    // println!("Now we see count {:?}", &vertices.iter().count());
 
     // each mesh in models has fields :
     // positions, normals, texcoords, indices, material_id
-
-
-
     println!("# of models: {}", models.len());
     println!("# of materials: {}", materials.len());
-
-
-
     println!("aeou {:?}", models[0]);
     println!("bsnth {:?}", models[1]);
 
